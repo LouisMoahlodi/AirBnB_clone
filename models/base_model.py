fb26dabca57a22f36294c6a0c7d128f97844b329
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2024-03-11 19:48:44 +0200

    removed strftime from kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -42,8 +42,8 @@ class BaseModel:
         else:
             # If kwargs is empty, create id and created_at attributes
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
-            self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
         # Generate a unique UUID for the instance
         self.id = str(uuid.uuid4())
