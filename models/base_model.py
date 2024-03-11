ff5dabb026c39efdf04ded5f7f88f18244927bad
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2024-03-11 21:19:14 +0200

    Test different code

--- models/base_model.py
+++ models/base_model.py
@@ -26,22 +26,36 @@ class BaseModel:
         """
 
         # Check if kwargs is not empty
-        if kwargs:
-            # Iterate through the key-value pairs in kwargs
-            for attr, value in kwargs.items():
-                # Convert created_at and updated_at strings to datetime objects
-                if attr in ['created_at', 'updated_at']:
-                    date_obj = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    self.__dict__[attr] = value
-
-                else: 
-                    # Set attribute dynamically
-                    self.__dict__[attr] = value
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
         else:
-            # If kwargs is empty, create id and created_at attributes
-            self.__dict__['id'] = str(uuid.uuid4())
-            self.__dict__['created_at'] = datetime.now()
-            self.__dict__['updated_at'] = datetime.now()
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        # if kwargs:
+        #     # Iterate through the key-value pairs in kwargs
+        #     for attr, value in kwargs.items():
+        #         # Convert created_at and updated_at strings to datetime objects
+        #         if attr in ['created_at', 'updated_at']:
+        #             date_obj = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+        #             self.__dict__[attr] = value
+
+        #         else: 
+        #             # Set attribute dynamically
+        #             self.__dict__[attr] = value
+        # else:
+        #     # If kwargs is empty, create id and created_at attributes
+        #     self.__dict__['id'] = str(uuid.uuid4())
+        #     self.__dict__['created_at'] = datetime.now()
+        #     self.__dict__['updated_at'] = datetime.now()
 
         # Generate a unique UUID for the instance
         self.id = str(uuid.uuid4())
