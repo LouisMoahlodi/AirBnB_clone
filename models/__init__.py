ac0ef921b8caf54f0428d809392fddee9d1487cc
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2024-03-12 18:23:13 +0200

    taske updated

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3
+
 # Import specific modules or symbols to make them accessible
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
 # Create the variable storage, an instance of FileStorage
 storage  = FileStorage()
