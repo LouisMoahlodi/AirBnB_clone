756782cc9c972f9a2a3aba2c5123ea76c06f9592
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2024-03-16 01:54:32 +0200

    Update to the base model class and storage.

--- models/base_model.py
+++ models/base_model.py
@@ -45,6 +45,7 @@ class BaseModel:
             self.__dict__['id'] = str(uuid.uuid4())
             self.__dict__['created_at'] = datetime.now()
             self.__dict__['updated_at'] = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """
@@ -61,7 +62,7 @@ class BaseModel:
         """
         # Update update time to current time
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """
