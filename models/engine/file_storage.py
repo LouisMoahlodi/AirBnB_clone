bbad56f82878fa778303a66b4e254f38fc75c638
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2024-03-15 15:54:59 +0200

    Updated reload function in storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -30,9 +30,18 @@ class FileStorage:
                  FileStorage.__objects.items()}, f)
 
     def reload(self):
-        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        """ Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
             with open(FileStorage.__file_path, 'r') as f:
-                FileStorage.__objects = json.load(f)
+                data = json.load(f)
+                for key, value in data.items():
+                    # Get the class name and ID from the key
+                    class_name, obj_id = key.split('.')
+                    # Create an instance of the class using eval
+                    obj_cls = eval(class_name)
+                    # Create a new instance of the class with the attributes from the loaded data
+                    obj = obj_cls(**value)
+                    # Add the object to __objects
+                    FileStorage.__objects[key] = obj
         except FileNotFoundError:
             pass
