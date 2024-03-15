9dabd0d64a1fbff81c1253cf88b1a789e63718be
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2024-03-15 22:15:57 +0200

    Update models and storage.

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,6 @@
-#!/usr/bin/python3
 import uuid
 from datetime import datetime
-import models
-
+from models import storage
 
 class BaseModel:
     """
@@ -61,8 +59,8 @@ class BaseModel:
         """
         # Update update time to current time
         self.updated_at = datetime.now()
-        models.storage.new(self)
-        models.storage.save()
+        storage.new(self)  # Call new method on storage to add the new object
+        storage.save()     # Call save method of storage to serialize the objects
 
     def to_dict(self):
         """
@@ -81,3 +79,86 @@ class BaseModel:
         obj_dict['updated_at'] = self.updated_at.isoformat()
 
         return obj_dict
+
+# import uuid
+# from datetime import datetime
+# import models
+
+
+# class BaseModel:
+#     """
+#     BaseModel class represents a base model for other classes.
+
+#     Attributes:
+#         id (str): Unique identifier for each instance.
+#         created_at (datetime): Time when an instance is created.
+#         updated_at (datetime): Time when an instance is updated.
+
+#     Methods:
+#         __init__: Initializes a new BaseModel instance.
+#         __str__: Returns a string representation of the BaseModel instance.
+#         save: Updates the updated_at attribute with the current datetime.
+#         to_dict: Returns a dictionary representation of the BaseModel instance.
+#     """
+
+#     def __init__(self, *args, **kwargs):
+#         """
+#         Initializes a new BaseModel instance.
+
+#         Sets the id attribute to a unique UUID, created_at and updated_at
+#         attributes to the current datetime.
+#         """
+
+#         # Check if kwargs is not empty
+#         if kwargs:
+#             # Iterate through the key-value pairs in kwargs
+#             for attr, value in kwargs.items():
+#                 # Convert created_at and updated_at strings to datetime objects
+#                 if attr in ['created_at', 'updated_at']:
+#                     date_obj = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+#                     self.__dict__[attr] = value
+
+#                 else:
+#                     # Set attribute dynamically
+#                     self.__dict__[attr] = value
+#         else:
+#             # If kwargs is empty, create id and created_at attributes
+#             self.__dict__['id'] = str(uuid.uuid4())
+#             self.__dict__['created_at'] = datetime.now()
+#             self.__dict__['updated_at'] = datetime.now()
+
+#     def __str__(self):
+#         """
+#         Returns a string representation of the BaseModel instance.
+
+#         Returns:
+#             str: String representation of the BaseModel instance.
+#         """
+#         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+#     def save(self):
+#         """
+#         Updates the updated_at attribute with the current datetime.
+#         """
+#         # Update update time to current time
+#         self.updated_at = datetime.now()
+#         models.storage.new(self)
+#         models.storage.save()
+
+#     def to_dict(self):
+#         """
+#         Returns a dictionary representation of the BaseModel instance.
+
+#         Returns:
+#         dict: Dictionary representation of the BaseModel instance.
+#         """
+#         # Include class name in the dictionary
+#         obj_dict = {'__class__': self.__class__.__name__}
+#         # Add instance attributes to the dictionary
+#         obj_dict.update(self.__dict__)
+#         # Convert creation time to ISO format string
+#         obj_dict['created_at'] = self.created_at.isoformat()
+#         # Convert update time to ISO format string
+#         obj_dict['updated_at'] = self.updated_at.isoformat()
+
+#         return obj_dict
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3
+
 """Module for FileStorage class."""
 
 import json
 import os
 import datetime
+from models.base_model import BaseModel
 
 
 class FileStorage:
@@ -19,23 +20,60 @@ class FileStorage:
 
     def new(self, obj):
         """Add a new object to the storage."""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        if obj is not None:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            FileStorage.__objects[key] = obj
 
     def save(self):
         """Serialize __objects to the JSON file __file_path."""
         with open(FileStorage.__file_path, 'w') as f:
-            json.dump(
-                {key: value for key, value in
-                 FileStorage.__objects.items()}, f)
+            obj_dict = {key: value.to_dict() for key, value in FileStorage.__objects.items()}
+            json.dump(obj_dict, f)
 
     def reload(self):
         """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
-            with open(FileStorage.__file_path, 'r') as f:
+            if not os.path.isfile(FileStorage.__file_path):
+                return
+            with open(FileStorage.__file_path, 'r', encoding="utf-8") as f:
                 FileStorage.__objects = json.load(f)
         except FileNotFoundError:
-            pass
+            FileStorage.__objects = {}
+# import json
+# import os
+# import datetime
+
+
+# class FileStorage:
+#     """Class for serializing instances to a JSON file and
+#     deserializing JSON file to instances."""
+
+#     __file_path = "file.json"
+#     __objects = {}
+
+#     def all(self):
+#         """Return a dictionary of all objects."""
+#         return FileStorage.__objects
+
+#     def new(self, obj):
+#         """Add a new object to the storage."""
+#         key = "{}.{}".format(obj.__class__.__name__, obj.id)
+#         FileStorage.__objects[key] = obj
+
+#     def save(self):
+#         """Serialize __objects to the JSON file __file_path."""
+#         with open(FileStorage.__file_path, 'w') as f:
+#             json.dump(
+#                 {key: value.to_dict() for key, value in
+#                  FileStorage.__objects.items()}, f)
+
+#     def reload(self):
+#         """Deserialize the JSON file __file_path to __objects, if it exists."""
+#         try:
+#             with open(FileStorage.__file_path, 'r') as f:
+#                 FileStorage.__objects = json.load(f)
+#         except FileNotFoundError:
+#             FileStorage.__objects = {}
             
     # def reload(self):
     #     """Deserialize the JSON file __file_path to __objects, if it exists."""
