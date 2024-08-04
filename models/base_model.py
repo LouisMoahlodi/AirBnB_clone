5fa1d6808b0340a72e46baf19a2b561cfc332d85
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2024-08-04 18:42:21 +0200

    Revert base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,3 +1,4 @@
+
 import uuid
 from datetime import datetime
 
@@ -24,17 +25,24 @@ class BaseModel:
         Sets the id attribute to a unique UUID, created_at and updated_at
         attributes to the current datetime.
         """
+        from models import storage
         # Check if kwargs is not empty
         if kwargs:
+            # Iterate through the key-value pairs in kwargs
             for attr, value in kwargs.items():
+                # Convert created_at and updated_at strings to datetime objects
                 if attr in ['created_at', 'updated_at']:
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                setattr(self, attr, value)
+                    date_obj = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__[attr] = value
+
+                else:
+                    # Set attribute dynamically
+                    self.__dict__[attr] = value
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            from models import storage
+            # If kwargs is empty, create id and created_at attributes
+            self.__dict__['id'] = str(uuid.uuid4())
+            self.__dict__['created_at'] = datetime.now()
+            self.__dict__['updated_at'] = datetime.now()
             storage.new(self)
 
     def __str__(self):
@@ -51,6 +59,7 @@ class BaseModel:
         Updates the updated_at attribute with the current datetime.
         """
         from models import storage
+        # Update update time to current time
         self.updated_at = datetime.now()
         storage.save()
 
@@ -59,10 +68,15 @@ class BaseModel:
         Returns a dictionary representation of the BaseModel instance.
 
         Returns:
-            dict: Dictionary representation of the BaseModel instance.
+        dict: Dictionary representation of the BaseModel instance.
         """
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
+        # Include class name in the dictionary
+        obj_dict = {'__class__': self.__class__.__name__}
+        # Add instance attributes to the dictionary
+        obj_dict.update(self.__dict__)
+        # Convert creation time to ISO format string
         obj_dict['created_at'] = self.created_at.isoformat()
+        # Convert update time to ISO format string
         obj_dict['updated_at'] = self.updated_at.isoformat()
+
         return obj_dict
